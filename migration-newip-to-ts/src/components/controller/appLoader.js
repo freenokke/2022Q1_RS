d5e754968e80ef83c23ffb81bb4646dd4bee0fc0
freenokke <kosmatkov94@gmail.com>
2022-06-30 14:21:25 +0300

    feat: add API key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '230d48c5013d4c00bef47cc7a53124e2', // получите свой ключ https://newsapi.org/
         });
     }
 }
