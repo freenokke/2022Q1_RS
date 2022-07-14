e3130aa434e68e033cde00d8f3d36801891a5739
freenokke <kosmatkov94@gmail.com>
2022-07-14 13:57:37 +0300

    feat: implement typescript type for JSON

---
+++ online-store/src/assets/components/additional/types/types.ts
@@ -0,0 +1,12 @@
+export type GoodsData = {
+    name: string;
+    brand: string;
+    img: string;
+    parameters: { diameter: string; ET: number; pcd: string; wide: string };
+    count: number;
+    purchaseQuantity: number;
+    color: string;
+    release: number;
+    pcd: string;
+    price: string;
+};
