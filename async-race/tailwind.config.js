f3d63bd51cb41c6f64437fc95563014973091f53
freenokke <kosmatkov94@gmail.com>
2022-07-27 15:32:15 +0300

    init: start async-race task

---
+++ async-race/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    important: true,
+    content: ['./src/*.{ts,js}', './src/*.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
