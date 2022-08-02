4d2b27dbacfc5bf583357c4b0cf69320b4f58b97
freenokke <kosmatkov94@gmail.com>
2022-08-02 12:19:32 +0300

    feat: create Control class

---
+++ async-race/src/components/Control.ts
@@ -0,0 +1,28 @@
+class Control<NodeType extends HTMLElement = HTMLElement> {
+  public node: NodeType;
+
+  constructor(
+    parentNode: HTMLElement | null,
+    tagName = 'div',
+    className = '',
+    content = ''
+  ) {
+    const el = document.createElement(tagName);
+    el.className = className;
+    el.textContent = content;
+    if (parentNode) {
+      parentNode.append(el);
+    }
+    this.node = el as NodeType;
+  }
+
+  destroy(): void {
+    this.node.remove();
+  }
+
+  clear(): void {
+    this.node.innerHTML = '';
+  }
+}
+
+export default Control;
