fc887e93a3f83497498ad7fbbd492b53df8f26d7
freenokke <kosmatkov94@gmail.com>
2022-08-02 12:54:52 +0300

    feat: impement methods to send a request to the server

--- async-race/src/components/controller/AppController.ts
+++ async-race/src/components/controller/AppController.ts
@@ -7,6 +7,24 @@ class AppController {
     this.model = new AppModel();
   }
 
+  public async deleteCar(id: number) {
+    await this.model.deleteCar(id);
+    const cars = await this.model.getCars();
+    return cars;
+  }
+
+  public async createCar(parameters: { name: string; color: string }) {
+    const cars = await this.model.createCar(parameters);
+    return cars;
+  }
+
+  public async updateCar(
+    id: number,
+    parameters: { name: string; color: string }
+  ) {
+    const cars = await this.model.updateCar(id, parameters);
+    return cars;
+  }
 }
 
 export default AppController;
--- async-race/src/components/model/AppModel.ts
+++ async-race/src/components/model/AppModel.ts
@@ -16,6 +16,44 @@ class AppModel {
     return this.carsInGarage;
   }
 
+  public async getCar(id: number) {
+    const res = await fetch(`${this.domain}/${Path.GARAGE}/${id}`);
+    const car = await res.json();
+    return car;
+  }
+
+  public async deleteCar(id: number) {
+    await fetch(`${this.domain}/${Path.GARAGE}/${id}`, {
+      method: 'DELETE',
+    });
+  }
+
+  public async createCar(parameters: { name: string; color: string }) {
+    await fetch(`${this.domain}/${Path.GARAGE}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(parameters),
+    });
+    const cars = this.getCars();
+    return cars;
+  }
+
+  public async updateCar(
+    id: number,
+    parameters: { name: string; color: string }
+  ) {
+    await fetch(`${this.domain}/${Path.GARAGE}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(parameters),
+    });
+    const cars = this.getCars();
+    return cars;
+  }
 }
 
 export default AppModel;
