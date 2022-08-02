0952a62648680647308dd269c7791e777c331b63
freenokke <kosmatkov94@gmail.com>
2022-08-02 12:13:23 +0300

    refactor: add settings to configs

--- async-race/tailwind.config.js
+++ async-race/tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+const { default: plugin } = require('tailwindcss');
+const pluginTail = require('tailwindcss/plugin');
+
 module.exports = {
-    important: true,
-    content: ['./src/*.{ts,js}', './src/*.html'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  // important: true,
+  content: ['./src/*.{ts,js}', './src/**/*.{ts,js}', './src/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    pluginTail(function ({ addComponents }) {
+      addComponents({
+        '.btn': {
+          padding: '.5rem 1rem',
+          borderRadius: '.25rem',
+          fontWeight: '600',
+        },
+        '.btn-blue': {
+          backgroundColor: '#3490dc',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#2779bd',
+          },
+        },
+        '.btn-red': {
+          backgroundColor: '#e3342f',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#cc1f1a'
+          },
+        },
+      });
+    }),
+  ],
 };
